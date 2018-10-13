34a1a264eb8c97eab907f439215750211ed264d4
Christopher <chris.ayling@blackbook.ai>
2018-10-13 15:02:17 +1000

    socket hello world added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,22 @@
 import Layout from '../components/layout'
+import io from 'socket.io-client'
 
 class Index extends React.Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            socket: null
+        }
     }
 
     componentDidMount = () => {
-
+        const socket = io("localhost:3000")
+        socket.on('hello_world', (data) => {
+            console.log(data);
+            
+        })
+        this.setState()
     }
 
     render() {
