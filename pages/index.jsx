46ff5ddd668f6f6ddf9005c1ba8d63bc7362ed79
Christopher <chris.ayling@blackbook.ai>
2018-10-14 14:40:56 +1000

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -93,7 +93,7 @@ class Index extends React.Component {
               ]
               console.log(prediction);
               return {
-                data: train.slice(Math.max(train.length - 3, 1)),
+                data: newData,
                 prediction: prediction
               }
             })
